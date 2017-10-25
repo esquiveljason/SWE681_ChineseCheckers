4bd20455e322474c491bcd1a8626bf868ab6916c
Jason Esquivel <jesquive90@gmail.com>
2017-10-25 15:57:04 -0400

    finished login templace, adding loggin modules morgan and winston

--- server.js
+++ server.js
@@ -3,16 +3,63 @@ var fs = require('fs');
 var app = express();
 var https = require('https');
 var path = require('path');
+
 const port = 8000;
+const logDir = 'log';
+
+if(!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+var winston = require('winston');
+
+const tsFormat = () => (new Date()).toLocaleString() ;
+
+const logger = new (winston.Logger)({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(
+      {
+        timestamp: tsFormat,
+      }
+    ),
+    new winston.transports.File(
+      {
+      filename: `${logDir}/server.log`,
+      timestamp: tsFormat,
+      json: false
+      }
+    )
+  ]
+});
+
+const morgan = require('morgan');
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+}));
+
 
 app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client'));
+  response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/test', (request, response) => {
+  response.redirect('/');
 });
 
 app.get('/earth', (request, response) => {
-  response.sendFile(path.join(__dirname+'/images/earth.gif'));
+  response.sendFile(path.join(__dirname,'/public/images/earth.gif'));
 });
 
 app.get('*', (request, response) => {
@@ -30,5 +77,6 @@ https.createServer(options, app).listen(port, (err) => {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  logger.info('Server is starting......');
+  logger.info(`Server is listening on ${port}`);
 });
