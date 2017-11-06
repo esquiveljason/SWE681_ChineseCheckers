ecdf4659e7f0f3109fa1ac918aff2c982fda944b
Jason Esquivel <jesquive90@gmail.com>
2017-11-06 15:04:19 -0500

    Added Home page after login and registration, with score and logout

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,37 @@ var router = express.Router();
 
 // Get Homepage
 router.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/../public/index.html'));
-});
-router.get('/login', (request, response) => {
-  response.redirect('/');
-});
-
-router.post('/login', (request, response) => {
-  response.send(request.body.username +request.body.password);
+  response.sendFile(path.join(__dirname, '/../public/login.html'));
 });
 
 router.get('/register', (request, response) => {
   response.sendFile(path.join(__dirname,'/../public/register.html'));
 });
 
+router.get('/home', (request, response) => {
+  response.sendFile(path.join(__dirname, '/../public/home.html'));
+});
+
+router.get('/scores', (request, response) => {
+  response.sendFile(path.join(__dirname, '/../public/scores.html'));
+});
+
+router.get('/logout', (request, response) => {
+  response.redirect("/");
+});
+
+router.post('/login', (request, response) => {
+  response.redirect('/home');
+});
+
+router.post('/register_page', (request, response) => {
+  response.redirect('/register');
+});
+
 router.post('/register', (request, response) => {
-  response.send(request.body.firstname+request.body.lastname+
-  request.body.username+request.body.password+request.body.password2);
+  response.redirect('/home');
+  //response.send(request.body.firstname+request.body.lastname+
+  //request.body.username+request.body.password+request.body.password2);
 });
 router.get('/earth', (request, response) => {
   response.sendFile(path.join(__dirname,'/../public/images/earth.gif'));
